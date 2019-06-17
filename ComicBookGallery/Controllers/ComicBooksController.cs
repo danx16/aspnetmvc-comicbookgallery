5d5f6916706f626f1c56fc26e865c6858626825f
dan <root.danx20@gmail.com>
2019-06-17 15:02:49 +0800

    Added Design files

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,35 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        // GET: ComicBooks
-        public ActionResult Index()
+        //public string Detail()
+        //{
+        //    return "Hello from comic books controller";
+        //}
+
+        public ActionResult Details()
         {
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            //{
+            //    return Redirect("/");
+            //}
+            //return Content("hello from comic books controller!");
+
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
+
+
             return View();
         }
     }
