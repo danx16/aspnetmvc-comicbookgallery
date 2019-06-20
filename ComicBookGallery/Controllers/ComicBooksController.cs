75580b60b625831b64356c8ed26e48df2f4d4bd5
dan <root.danx20@gmail.com>
2019-06-20 09:48:13 +0800

    Added Comic Books List View

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -17,6 +17,14 @@ namespace ComicBookGallery.Controllers
             _comicBookRepository = new ComicBookRepository();
         }
 
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
         public ActionResult Details(int? id)
         {
             if (id == null)
